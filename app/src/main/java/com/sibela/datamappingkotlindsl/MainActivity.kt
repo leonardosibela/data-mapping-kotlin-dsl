4ceb57e66e14bc15a002d1499d5fcf461c8d57b9
leonardosibela <leonardo_sibela@hotmail.com>
2021-05-02 12:29:06 -0300

    Using Kotlin DSL

--- app/src/main/java/com/sibela/datamappingkotlindsl/MainActivity.kt
+++ app/src/main/java/com/sibela/datamappingkotlindsl/MainActivity.kt
@@ -2,11 +2,18 @@ package com.sibela.datamappingkotlindsl
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.sibela.datamappingkotlindsl.Student.Companion.student
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val student = student {
+            name = "Alex"
+            standard = 10
+            rollNumber = 720
+        }
     }
 }
