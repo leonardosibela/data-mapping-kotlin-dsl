a51d9e69c84658cf94d64d9791e2c5d26b9b3e1b
leonardosibela <leonardo_sibela@hotmail.com>
2021-05-02 12:28:06 -0300

    Adding DSL builder

--- app/src/main/java/com/sibela/datamappingkotlindsl/Student.kt
+++ app/src/main/java/com/sibela/datamappingkotlindsl/Student.kt
@@ -1,7 +1,21 @@
 package com.sibela.datamappingkotlindsl
 
 class Student(
-    val name: String,
+    val name: String?,
     val standard: Int,
     val rollNumber: Int
-)
+) {
+
+    private constructor(builder: Builder) : this(builder.name, builder.standard, builder.rollNumber)
+
+    companion object {
+        inline fun student(block: Builder.() -> Unit) = Builder().apply(block).build()
+    }
+
+    class Builder {
+        var name: String? = null
+        var standard: Int = 0
+        var rollNumber: Int = 0
+        fun build() = Student(this)
+    }
+}
